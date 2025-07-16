296b315e50b15a21302f64ae4e570e25bd642e1a
Ture Claussen <ture.cl.business@gmail.com>
2025-07-17 00:41:55 +0200

    Added more memory info

--- project/deployment/src/log.cpp
+++ project/deployment/src/log.cpp
@@ -9,14 +9,33 @@ void logDebugln()
 {
 }
 
+#include "esp_heap_caps.h"
+
 void printHeapInfo()
 {
     Serial.print("Heap: ");
     Serial.println(ESP.getHeapSize());
     Serial.print("Free Heap: ");
     Serial.println(ESP.getFreeHeap());
+
     Serial.print("PSRAM: ");
     Serial.println(ESP.getPsramSize());
     Serial.print("Free PSRAM: ");
     Serial.println(ESP.getFreePsram());
+
+    // Extended SRAM info
+    multi_heap_info_t heapInfo;
+    heap_caps_get_info(&heapInfo, MALLOC_CAP_INTERNAL); // internal RAM (SRAM)
+
+    Serial.println("--- Internal SRAM Info ---");
+    Serial.print("Total Allocated: ");
+    Serial.println(heapInfo.total_allocated_bytes);
+    Serial.print("Total Free: ");
+    Serial.println(heapInfo.total_free_bytes);
+    Serial.print("Largest Free Block: ");
+    Serial.println(heapInfo.largest_free_block);
+
+    // Optional: stack info (only useful if you are checking task-level usage)
+    Serial.print("Free Stack (main task): ");
+    Serial.println(uxTaskGetStackHighWaterMark(NULL));
 }
--- project/deployment/src/log.hpp
+++ project/deployment/src/log.hpp
@@ -1,2 +1,3 @@
 void logDebug();
 void logDebugln();
+void printHeapInfo();
